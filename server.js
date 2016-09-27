d8d8bc380318c0a9ec661b2d4be2e08a4e268d1c
lalichan <lalichanayillumalil@gmail.com>
2016-09-27 15:11:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one',function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req, res) {
- res.send('Article two requested and willbe served here');
+res.sendFile(path.join(__dirname, 'ui', 'article-ontwo.html'));
 });
 app.get('/article-three',function (req, res) {
  res.send('Article three requested and willbe served here');
