1bb2edec35a3346dfded19371492547fc4573709
lalichan <lalichanayillumalil@gmail.com>
2016-09-28 22:01:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+ app.get('/', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+
 var articleOne= {
     title:"Article1! Mathew Thomas",
     heading:"Article One",
