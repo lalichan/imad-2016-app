88003f7dcfed5f870747d13a8023ab0e45994c9d
lalichan <lalichanayillumalil@gmail.com>
2016-09-29 12:21:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ var articles= {
 function createTemplate (date) {
     var title = data.title;
     var heading = data.heading;
-    var date = data.date;
+ 
     var content=data.content;
     
 var htmlTemplate=`
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
-    var articleName = req.params-articleName;
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
