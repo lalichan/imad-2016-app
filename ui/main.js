365c56b6dcd1ecf800d60caea3025b954c35b5dd
lalichan <lalichanayillumalil@gmail.com>
2016-10-03 11:55:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('Newvalue'
 );
 
-element.innerHTML=Newvalue;
+element.innerHTML='Newvalue';
 
 //change the image
 var img = document.getElementById('madi');
