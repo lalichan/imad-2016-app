450ec7b79e638d52f9cd1d5d16176236930dcca5
lalichan <lalichanayillumalil@gmail.com>
2016-10-06 21:17:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,10 +31,11 @@ submit.onclick = function() {
     //capture the list of names and render it as list
    var name = ['name1','name2','name3','name4']; 
    var list = '';
-   for (var I=0, I < names.length, I++) {
-    list : '<li>' + names[I] + '</li>'; 
+   for (var I=0; I < names.length;  I++) {
+    list == '<li>' + names[I] + '</li>'; 
     }
 
+   
    var ul = document.getElementById('namelist');
    ul.innerHTML = list;
 };
